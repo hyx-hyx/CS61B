47df8a8f85e00d8292b59fc29ab1e03273068c57
hyx-hyx <315024424@qq.com>
2025-03-27 15:13:20 +0800

    去掉StdAudio.play(first);因为GuitarString只是仿真，不用发出声音。

--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -22,7 +22,7 @@ public class GuitarString {
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
         int capacity = (int) Math.round(SR / frequency);
-        buffer = new ArrayDeque<Double>();
+        buffer = new ArrayDeque<>();
         for (int i = 0; i < capacity; ++i) {
             buffer.addLast(0.0);
         }
@@ -49,11 +49,9 @@ public class GuitarString {
      * the Karplus-Strong algorithm.
      */
     public void tic() {
-
         double first = buffer.removeFirst();
         double second = buffer.get(0);
         buffer.addLast((first + second) / 2 * DECAY);
-        StdAudio.play(first);
     }
 
     /* Return the double at the front of the buffer. */
